3f05d14aa3878a85ce7540d7d6673e90ac3ff33c
viogami <2654613995@qq.com>
2024-01-25 21:09:33 +0800

    fix1

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ var port = os.Getenv("PORT")
 func main() {
 	server := server.New()
 	err := server.Run(port)
+	log.Println("HTTP server is running on port", port)
 	if err != nil {
 		log.Printf("Error starting server: %v\n", err)
 	}
