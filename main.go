4cd89f6302bb89276f6c21b4945119a9182d0d07
viogami <2654613995@qq.com>
2024-01-25 23:39:47 +0800

    rollback

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"Gobot-vio/server"
 	"Gobot-vio/tgbot"
-	"log"
 	"os"
 )
 
@@ -12,12 +10,4 @@ var port = os.Getenv("PORT")
 func main() {
 	//创建一个tgbot
 	tgbot.CreateTgbot()
-
-	//创建一个http server
-	server := server.New()
-	err := server.Run(port)
-	log.Println("HTTP server is running on port:", port)
-	if err != nil {
-		log.Printf("Error starting server: %v\n", err)
-	}
 }
