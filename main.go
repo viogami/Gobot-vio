dcdf57441cdf568dc6d959f17bb01ae5b4f1c1dd
viogami <2654613995@qq.com>
2024-01-26 00:10:17 +0800

    fix11

--- main.go
+++ main.go
@@ -3,21 +3,16 @@ package main
 import (
 	"Gobot-vio/server"
 	"Gobot-vio/tgbot"
-	"log"
 	"os"
 )
 
-var port = os.Getenv("PORT")
-
 func main() {
+	var port = os.Getenv("PORT")
+
 	//创建一个tgbot
 	tgbot.CreateTgbot()
 
 	//创建一个http server
 	server := server.New()
-	err := server.Run(port)
-	log.Println("HTTP server is running on port:", port)
-	if err != nil {
-		log.Printf("Error starting server: %v\n", err)
-	}
+	server.Run(port)
 }
--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -14,15 +15,19 @@ func New() *Server {
 	}
 }
 
-func (s *Server) Run(port string) error {
+func (s *Server) Run(port string) {
 	// 设置 /post 路径的 HTTP 处理函数
 	s.mux.HandleFunc("/post", handlePost)
 	// 处理WebSocket请求的路由
 	s.mux.HandleFunc("/ws", handleWebSocket)
 	// 启动 Web 服务器监听 port 端口
-	err := http.ListenAndServe(":"+port, s.mux)
-
-	return err
+	go func() {
+		err := http.ListenAndServe(":"+port, s.mux)
+		log.Println("HTTP server is running on port:", port)
+		if err != nil {
+			log.Printf("Error starting server: %v\n", err)
+		}
+	}()
 }
 
 // Close 方法用于关闭服务器
