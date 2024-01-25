a43f4f62631a83a55f676ff349a2ee55433babf1
viogami <2654613995@qq.com>
2024-01-25 23:14:22 +0800

    fix7

--- main.go
+++ main.go
@@ -10,14 +10,14 @@ import (
 var port = os.Getenv("PORT")
 
 func main() {
-	go func() {
-		err := server.Run(port)
-		log.Println("HTTP server is running on port:", port)
-		if err != nil {
-			log.Printf("Error starting server: %v\n", err)
-		}
-	}()
-
 	//创建一个tgbot
 	tgbot.CreateTgbot()
+
+	//创建一个http server
+	server := server.New()
+	err := server.Run(port)
+	log.Println("HTTP server is running on port:", port)
+	if err != nil {
+		log.Printf("Error starting server: %v\n", err)
+	}
 }
