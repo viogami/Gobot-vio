09c4a00c3b67bfeeb52b2318cf009940336b8948
viogami <2654613995@qq.com>
2024-01-25 23:54:57 +0800

    fix10

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"Gobot-vio/server"
 	"Gobot-vio/tgbot"
+	"log"
 	"os"
 )
 
@@ -11,4 +13,11 @@ func main() {
 	//创建一个tgbot
 	tgbot.CreateTgbot()
 
+	//创建一个http server
+	server := server.New()
+	err := server.Run(port)
+	log.Println("HTTP server is running on port:", port)
+	if err != nil {
+		log.Printf("Error starting server: %v\n", err)
+	}
 }
