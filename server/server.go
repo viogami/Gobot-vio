bc2ef27d97a1594f7653f30b89a33b227dc31129
viogami <2654613995@qq.com>
2024-01-25 22:52:20 +0800

    fix3

--- server/server.go
+++ server/server.go
@@ -15,10 +15,10 @@ func New() *Server {
 }
 
 func (s *Server) Run(port string) error {
-	// 设置 / 路径
-	s.mux.HandleFunc("/", handleHome)
 	// 设置 /post 路径的 HTTP 处理函数
 	s.mux.HandleFunc("/post", handlePost)
+	// 处理WebSocket请求的路由
+	s.mux.HandleFunc("/ws", handleWebSocket)
 	// 启动 Web 服务器监听 port 端口
 	err := http.ListenAndServe(":"+port, s.mux)
 	return err
