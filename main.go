aef49cd0c4c4c778229893faa478f84d2356248c
viogami <2654613995@qq.com>
2024-06-15 00:36:12 +0800

    test env

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"log"
+
 	"github.com/viogami/Gobot-vio/config"
 	"github.com/viogami/Gobot-vio/server"
 )
 
 func main() {
 	env := config.GetEnv()
+	log.Println(env)
 	server.Run(env.PORT)
 }
